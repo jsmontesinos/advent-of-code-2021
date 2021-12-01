1b3853d1a4850cf3492dc479f3cf5b79ed46f806
Juan Sánchez <juan.sanchez@xepient.com>
2021-12-01 20:38:47 +0100

    Solve day1 puzzle

--- __tests__/day1.spec.ts
+++ __tests__/day1.spec.ts
@@ -1,7 +1,24 @@
-import { day1 } from '../src/day1';
+import {
+  depthIncreaseCounter,
+  threeMeasurementWindowIncreaseCounter,
+} from '../src/day1';
 
-describe('day1, part1', () => {
-  it('should return an empty string array', async () => {
-    expect(await day1()).toEqual([]);
+describe('day1', () => {
+  describe('part 1', () => {
+    it('should return how many times the depth increases', async () => {
+      const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+      expect(await depthIncreaseCounter(input)).toEqual(7);
+    });
+  });
+  describe('part 2', () => {
+    it('should return how many times the three measurement window increases', async () => {
+      const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+      expect(await threeMeasurementWindowIncreaseCounter(input)).toEqual(5);
+    });
+
+    it('should return how many times the three measurement window increases in [1, 2, 4, 3, 4, 5, 3, 3]', async () => {
+      const input = [1, 2, 4, 3, 4, 5, 3, 3];
+      expect(await threeMeasurementWindowIncreaseCounter(input)).toEqual(3);
+    });
   });
 });
--- src/day1.ts
+++ src/day1.ts
@@ -1,3 +1,31 @@
+import { readyDayFixture } from './day-common';
+
+export function depthIncreaseCounter(input: number[]): number {
+  return input.reduce(
+    (acc, current, currentIndex) =>
+      currentIndex > 0 && current - input[currentIndex - 1] > 0 ? acc + 1 : acc,
+    0,
+  );
+}
+
+export function threeMeasurementWindowIncreaseCounter(input: number[]): number {
+  return depthIncreaseCounter(
+    input.reduce(
+      (acc, current, index, array) =>
+        index <= array.length - 3
+          ? [...acc, current + array[index + 1] + array[index + 2]]
+          : acc,
+      [],
+    ),
+  );
+}
+
 export async function day1(): Promise<string[]> {
-  return Promise.resolve([]);
+  const lines = await readyDayFixture(1);
+  return [
+    depthIncreaseCounter(lines.map((line) => parseInt(line, 10)))?.toString(),
+    threeMeasurementWindowIncreaseCounter(
+      lines.map((line) => parseInt(line, 10)),
+    )?.toString(),
+  ];
 }
