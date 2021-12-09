92d7e074966c760c9e5435d2da14669edb580b29
Juan Sánchez <juan.sanchez@xepient.com>
2021-12-09 21:28:57 +0100

    Day 9: solve part2

--- __tests__/day9.spec.ts
+++ __tests__/day9.spec.ts
@@ -1,10 +1,19 @@
 import {
+  calculateBasin,
   calculateRiskLevel,
   locateLowPoints,
+  multiplyThreeLargerBasins,
   parseToHeightMap,
 } from '../src/day9';
 
 describe('day 9', () => {
+  const example = [
+    '2199943210',
+    '3987894921',
+    '9856789892',
+    '8767896789',
+    '9899965678',
+  ];
   describe('part 1', () => {
     describe('locateLowPoints()', () => {
       it('should locate a low height position in string 9808', () => {
@@ -19,14 +28,7 @@ describe('day 9', () => {
         ]);
       });
       it('should localize low height points in day 9 example', () => {
-        const input = [
-          '2199943210',
-          '3987894921',
-          '9856789892',
-          '8767896789',
-          '9899965678',
-        ];
-        expect(locateLowPoints(parseToHeightMap(input))).toEqual([
+        expect(locateLowPoints(parseToHeightMap(example))).toEqual([
           { x: 1, y: 0, height: 1 },
           { x: 9, y: 0, height: 0 },
           { x: 2, y: 2, height: 5 },
@@ -34,16 +36,27 @@ describe('day 9', () => {
         ]);
       });
       it('should calculate risk level for day 9 example', () => {
-        const input = [
-          '2199943210',
-          '3987894921',
-          '9856789892',
-          '8767896789',
-          '9899965678',
-        ];
         expect(
-          calculateRiskLevel(locateLowPoints(parseToHeightMap(input))),
+          calculateRiskLevel(locateLowPoints(parseToHeightMap(example))),
         ).toBe(15);
+      });
+    });
+  });
+  describe('part 2', () => {
+    describe('calculateBasin()', () => {
+      it('should calculate top left basin for example', () => {
+        expect(
+          calculateBasin(parseToHeightMap(example), { x: 1, y: 0, height: 1 }),
+        ).toEqual([
+          { x: 1, y: 0, height: 1 },
+          { x: 0, y: 0, height: 2 },
+          { x: 0, y: 1, height: 3 },
+        ]);
+      });
+    });
+    describe('multiplyThreeLargerBasins', () => {
+      it('should return 1134 for example', () => {
+        expect(multiplyThreeLargerBasins(parseToHeightMap(example))).toBe(1134);
       });
     });
   });
--- src/day9.ts
+++ src/day9.ts
@@ -12,29 +12,77 @@ export function locateLowPoints(heightMap: HeightMap): HeightMapPosition[] {
   for (let i = 0; i < heightMap.length; i++) {
     for (let j = 0; j < heightMap[i].length; j++) {
       if (isLocalMinimum(heightMap, j, i)) {
-        lowPoints.push({ x: j, y: i, height: heightMap[i][j] });
+        lowPoints.push(getHeightMapPosition(heightMap, j, i));
       }
     }
   }
   return lowPoints;
 }
 
+function getHeightMapPosition(
+  heightMap: HeightMap,
+  x: number,
+  y: number,
+): HeightMapPosition {
+  return { x, y, height: heightMap[y][x] };
+}
+
 function isLocalMinimum(heightMap: HeightMap, x: number, y: number) {
   return (
-    (x === 0 || heightMap[y][x - 1] > heightMap[y][x]) &&
-    (x === heightMap[y].length - 1 || heightMap[y][x + 1] > heightMap[y][x]) &&
-    (y === 0 || heightMap[y - 1][x] > heightMap[y][x]) &&
-    (y === heightMap.length - 1 || heightMap[y + 1][x] > heightMap[y][x])
+    (isOutsideMap(heightMap, x - 1, y) ||
+      heightMap[y][x - 1] > heightMap[y][x]) &&
+    (isOutsideMap(heightMap, x + 1, y) ||
+      heightMap[y][x + 1] > heightMap[y][x]) &&
+    (isOutsideMap(heightMap, x, y - 1) ||
+      heightMap[y - 1][x] > heightMap[y][x]) &&
+    (isOutsideMap(heightMap, x, y + 1) || heightMap[y + 1][x] > heightMap[y][x])
   );
+}
+
+function isOutsideMap(heightMap: HeightMap, x: number, y: number) {
+  return x < 0 || y < 0 || x >= heightMap[0].length || y >= heightMap.length;
 }
 
 export function calculateRiskLevel(positions: HeightMapPosition[]): number {
   return positions.reduce((acc, current) => current.height + 1 + acc, 0);
 }
 
+export function calculateBasin(
+  heightMap: HeightMap,
+  position: HeightMapPosition,
+): HeightMapPosition[] {
+  const visited = heightMap.map((line) => line.map(() => false));
+  const recursiveCalculateBasinIfNeeded = (x: number, y: number) =>
+    !isOutsideMap(heightMap, x, y) && !visited[y][x] && heightMap[y][x] !== 9
+      ? recursiveCalculateBasin(getHeightMapPosition(heightMap, x, y))
+      : [];
+  const recursiveCalculateBasin = (recursivePosition: HeightMapPosition) => {
+    const { x, y } = recursivePosition;
+    visited[y][x] = true;
+    return [
+      getHeightMapPosition(heightMap, x, y),
+      ...recursiveCalculateBasinIfNeeded(x - 1, y),
+      ...recursiveCalculateBasinIfNeeded(x + 1, y),
+      ...recursiveCalculateBasinIfNeeded(x, y - 1),
+      ...recursiveCalculateBasinIfNeeded(x, y + 1),
+    ];
+  };
+
+  return recursiveCalculateBasin(position);
+}
+
+export function multiplyThreeLargerBasins(heightMap: HeightMap): number {
+  const lowerPoints = locateLowPoints(heightMap);
+  const allBasins = lowerPoints
+    .map((p) => calculateBasin(heightMap, p))
+    .sort((a, b) => a.length - b.length);
+  return allBasins.slice(-3).reduce((acc, current) => acc * current.length, 1);
+}
+
 export async function day9(): Promise<string[]> {
   const lines = await readyDayFixture(9);
   return [
     calculateRiskLevel(locateLowPoints(parseToHeightMap(lines))).toString(),
+    multiplyThreeLargerBasins(parseToHeightMap(lines)).toString(),
   ];
 }
