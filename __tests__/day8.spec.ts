48828526e9cbea86d3961f7d3121efa24d6e76d4
Juan Sánchez <juan.sanchez@xepient.com>
2021-12-09 19:35:40 +0100

    Day 8: solve part2

--- __tests__/day8.spec.ts
+++ __tests__/day8.spec.ts
@@ -1,8 +1,13 @@
-import { countUniqueSegmentInstances, decodeDisplay } from '../src/day8';
+import {
+  countUniqueSegmentInstances,
+  decodeAll,
+  decodeLine,
+  getSegmentDecoder,
+  getSegmentTable,
+} from '../src/day8';
 
-const simpleExampleInput = [
-  'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf',
-];
+const simpleExampleInput =
+  'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf';
 const exampleInput = [
   'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe',
   'edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc',
@@ -23,7 +28,7 @@ describe('day 8', () => {
     });
 
     it('should get 0 number of unique segment instances in simple example input', () => {
-      expect(countUniqueSegmentInstances(simpleExampleInput)).toBe(0);
+      expect(countUniqueSegmentInstances([simpleExampleInput])).toBe(0);
     });
 
     it('should detect 1 in a | ce cedbg cedba', () => {
@@ -57,12 +62,65 @@ describe('day 8', () => {
   });
 
   describe('part 2', () => {
-    it.skip('should decode all lines and return 5353 for simple example', () => {
-      expect(decodeDisplay(exampleInput)).toBe(5353);
-    });
+    describe('getSegmentTable', () => {
+      it('should infer segments for simpleExampleInput segment', () => {
+        const segmentTable = getSegmentTable(simpleExampleInput);
 
-    it.skip('should decode all lines and return 61229 for larger example', () => {
-      expect(decodeDisplay(exampleInput)).toBe(61229);
+        expect(segmentTable.get('a')).toBe('d');
+        expect(segmentTable.get('c')).toBe('a');
+        expect(segmentTable.get('f')).toBe('b');
+        expect(segmentTable.get('e')).toBe('g');
+        expect(segmentTable.get('d')).toBe('f');
+        expect(segmentTable.get('b')).toBe('e');
+        expect(segmentTable.get('g')).toBe('c');
+      });
+
+      it('should infer segments for exampleInput[3] segment', () => {
+        const segmentTable = getSegmentTable(exampleInput[3]);
+
+        expect(segmentTable.get('a')).toBe('d');
+        expect(segmentTable.get('c')).toBe('b');
+        expect(segmentTable.get('f')).toBe('c');
+        expect(segmentTable.get('e')).toBe('g');
+        expect(segmentTable.get('d')).toBe('a');
+        expect(segmentTable.get('b')).toBe('f');
+        expect(segmentTable.get('g')).toBe('e');
+      });
+    });
+    describe('getDecodingTable', () => {
+      it('should return decoding table for simple example', () => {
+        const decoder = getSegmentDecoder(simpleExampleInput);
+
+        expect(decoder('acedgfb')).toBe(8);
+        expect(decoder('cdfbe')).toBe(5);
+        expect(decoder('gcdfa')).toBe(2);
+        expect(decoder('fbcad')).toBe(3);
+        expect(decoder('dab')).toBe(7);
+        expect(decoder('cefabd')).toBe(9);
+        expect(decoder('cdfgeb')).toBe(6);
+        expect(decoder('eafb')).toBe(4);
+        expect(decoder('cagedb')).toBe(0);
+        expect(decoder('ab')).toBe(1);
+      });
+    });
+    describe('decodeDisplay', () => {
+      it('should decode all lines and return 5353 for simple example first line', () => {
+        expect(decodeLine(simpleExampleInput)).toBe(5353);
+      });
+
+      exampleInput.forEach((line) => {
+        it(`should return a numeric value for line ${line}`, () => {
+          expect(decodeLine(line)).toBeGreaterThan(0);
+        });
+      });
+
+      it('should decode all lines and return 5353 for simple example', () => {
+        expect(decodeAll([simpleExampleInput])).toBe(5353);
+      });
+
+      it('should decode all lines and return 61229 for larger example', () => {
+        expect(decodeAll(exampleInput)).toBe(61229);
+      });
     });
   });
 });
--- src/day8.ts
+++ src/day8.ts
@@ -14,12 +14,105 @@ function countUniqueSegmentInstancesInLine(line: string): number {
     .filter((digit) => [2, 7, 4, 3].includes(digit.length)).length;
 }
 
-// @ts-ignore
-export function decodeDisplay(input: string[]): number {
-  return 0;
+export function getSegmentTable(line: string): Map<string, string> {
+  const segmentTable = new Map<string, string>();
+  const uniqueSequence = line.split(' | ')[0].split(' ');
+  const one = uniqueSequence.find((s) => s.length === 2);
+  const seven = uniqueSequence.find((s) => s.length === 3);
+  const six = uniqueSequence.find(
+    (s) => s.length === 6 && subtractSequence(one, s).length > 0,
+  );
+  const four = uniqueSequence.find((s) => s.length === 4);
+  const eight = uniqueSequence.find((s) => s.length === 7);
+  segmentTable.set('a', subtractSequence(seven, one));
+  segmentTable.set('c', subtractSequence(eight, six));
+  const five = uniqueSequence.find(
+    (s) => s.length === 5 && sumSequences(s, segmentTable.get('c')).length > 5,
+  );
+  const nine = uniqueSequence.find(
+    (s) => s.length === 6 && s !== six && sumSequences(five, s).length < 7,
+  );
+  const zero = uniqueSequence.find(
+    (s) => s.length === 6 && s !== six && s !== nine,
+  );
+  segmentTable.set('e', subtractSequence(eight, nine));
+  segmentTable.set('d', subtractSequence(eight, zero));
+  segmentTable.set('f', subtractSequence(one, segmentTable.get('c')));
+  segmentTable.set(
+    'b',
+    subtractSequence(
+      four,
+      sumSequences(
+        segmentTable.get('c'),
+        segmentTable.get('f'),
+        segmentTable.get('d'),
+      ),
+    ),
+  );
+  segmentTable.set(
+    'g',
+    subtractSequence(
+      eight,
+      sumSequences(
+        segmentTable.get('c'),
+        segmentTable.get('b'),
+        segmentTable.get('e'),
+        segmentTable.get('a'),
+        segmentTable.get('f'),
+        segmentTable.get('d'),
+      ),
+    ),
+  );
+
+  return segmentTable;
+}
+
+function sumSequences(...sequences: string[]): string {
+  return [...new Set(sequences.flatMap((sequence) => sequence.split('')))].join(
+    '',
+  );
+}
+
+function subtractSequence(sequence: string, otherSequence: string): string {
+  return sequence
+    .split('')
+    .filter((s) => !otherSequence.includes(s))
+    .join('');
+}
+
+export function getSegmentDecoder(line: string): (input: string) => number {
+  const segmentTable = getSegmentTable(line);
+  const matchesAllSegments = (input: string, segments: string[]) =>
+    segments.every((s) => input.includes(segmentTable.get(s)));
+  return (input: string) =>
+    [
+      { segments: ['a', 'b', 'c', 'd', 'e', 'f', 'g'], value: 8 },
+      { segments: ['a', 'b', 'd', 'e', 'f', 'g'], value: 6 },
+      { segments: ['a', 'b', 'c', 'd', 'f', 'g'], value: 9 },
+      { segments: ['a', 'b', 'c', 'e', 'f', 'g'], value: 0 },
+      { segments: ['a', 'c', 'd', 'e', 'g'], value: 2 },
+      { segments: ['a', 'c', 'd', 'f', 'g'], value: 3 },
+      { segments: ['a', 'b', 'd', 'f', 'g'], value: 5 },
+      { segments: ['b', 'c', 'd', 'f'], value: 4 },
+      { segments: ['a', 'c', 'f'], value: 7 },
+      { segments: ['c', 'f'], value: 1 },
+    ].find((decoder) => matchesAllSegments(input, decoder.segments))?.value;
+}
+
+export function decodeLine(line: string): number {
+  const values = line.split(' | ')[1].split(' ');
+  const decode = getSegmentDecoder(line);
+  return parseInt(values.map((value) => decode(value).toString()).join(''), 10);
+}
+
+export function decodeAll(input: string[]): number {
+  return input.reduce((acc, current) => acc + decodeLine(current), 0);
 }
 
 export async function day8(): Promise<string[]> {
   const lines = await readyDayFixture(8);
-  return [countUniqueSegmentInstances(lines).toString()];
+  return [
+    countUniqueSegmentInstances(lines).toString(),
+    decodeAll(lines).toString(),
+  ];
 }
